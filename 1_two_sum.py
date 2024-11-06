6d2bee26aaaec73740e5c88f4ee5cbe2d85514ae
Shubham Dangwal <shdangwal@gmail.com>
2024-11-06 08:16:49 +0530

    - *.py from root folder
    as python solutions have been moved to python folder

--- 1_two_sum.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        d = {}
-        for ind, num in enumerate(nums):
-            comp = target - num
-            if num in d:
-                return [ind, d[num]]
-            else:
-                d[comp] = ind
